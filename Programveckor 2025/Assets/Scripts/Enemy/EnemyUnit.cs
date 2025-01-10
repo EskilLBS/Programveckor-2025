26b523a46fb686cb49c6bb801967e723672797d5
EskilLBS <eskil.hejenstedt@elev.ga.lbs.se>
2025-01-10 08:56:41 +0100

    Fixade dubbelhopp

--- Programveckor 2025/Assets/Scripts/Enemy/EnemyUnit.cs
+++ Programveckor 2025/Assets/Scripts/Enemy/EnemyUnit.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyUnit : UnitBase
 {
     [SerializeField] int damage;
 
     [HideInInspector] public bool spared;
 
+    [SerializeField] string playerName;
+    static string pName;
+
+    Rigidbody2D rb;
+    [SerializeField] float sparedLifeRetreatSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
+        pName = playerName;
         health = maxHealth;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public override void Attack()
@@ -43,6 +52,12 @@ public class EnemyUnit : UnitBase
         CombatManager.Instance.RemoveEnemyFromList(this);
         CombatManager.Instance.SetCurrentTarget(null);
 
+        Destroy(gameObject);
+    }
+
+    // Caleld when you spare the life of an enemty
+    public void OnSpared()
+    {
         Destroy(gameObject);
     }
 }
