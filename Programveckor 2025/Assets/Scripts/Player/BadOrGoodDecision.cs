2e4810699d44b5b368fd90bf0d2e911daa23ba18
Erik-Thorin <erik.thorin.stockholm@gmail.com>
2025-01-10 09:09:52 +0100

    changed some vaulues

--- Programveckor 2025/Assets/Scripts/Player/BadOrGoodDecision.cs
+++ Programveckor 2025/Assets/Scripts/Player/BadOrGoodDecision.cs
@@ -15,14 +15,17 @@ public class BadOrGoodDecision : MonoBehaviour
 
     public void BadDecision()
     {
+        //1 badness is added every time you make a bad decision
         badness += 1;
         print("Det var ett dåligt val!");
         
+        //You get a warning after 3 bad decision
         if (badness == 3)
         {
             print("Du har gjort flera dåliga val!!");
         }
 
+        //After you make 4 bad decisions, the world explodes
         if (badness == 4)
         {
             print("BOOM!!!!!!!!! DU SPRÄNGDE VÄRLDEN!");
@@ -30,14 +33,17 @@ public class BadOrGoodDecision : MonoBehaviour
     }
     public void GoodDecision()
     {
+        //1 goodness is added every time you make a good decision
         goodness += 1;
         print("Det var ett bra val!");
 
+        //You get a notification when you have made 3 good decisions
         if (goodness == 3)
         {
             print("Du har gjort flera bra val!!");
         }
 
+        //After you make 4 good decisions, you save the world
         if (goodness == 4)
         {
             print("DU RÄDDADE VÄRLDEN!!");
